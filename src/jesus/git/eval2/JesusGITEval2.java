2e6914f0aa27988e034138d047ac6607d1b47855
jesus <jesus@DESKTOP-K302692>
2023-05-26 01:47:48 +0200

    Commit Github-ejercicio10

---
+++ src/jesus/git/eval2/JesusGITEval2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jesus.git.eval2;
+
+/**
+ *
+ * @author jesus
+ */
+public class JesusGITEval2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Editando linea despues del primer commit");
+        System.out.println("Cambio para github por jesus lopez perez");
+    }
+    
+}
